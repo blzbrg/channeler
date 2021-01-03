35b6249cdaab0932196c2a4362c0b86c466708c3
Blzbrg <randomparanoid@gmail.com>
2021-01-02 23:27:58 -0500

    Make port configurable, via environment var

--- command.sh
+++ command.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 
+# Connect to a channeler remote-control server on localhost
+
+# Uses TCP over port CHANNELER_PORT. One suggestion to customize this is writing a shell
+# script which contains the single line
+# CHANNELER_PORT=9000 ./command.sh $@
+
 # take the first arg to avoid having a linebreak before it
 msg="$1"
 shift
@@ -17,19 +23,27 @@ msg=$msg"\n\n"
 eot=`printf "\x04"`
 msg=$msg$eot
 
+
+if [ -n "$CHANNELER_PORT" ]
+then
+    port="$CHANNELER_PORT"
+else
+    port="9001" # default port
+fi
+
 if command -v ncat 2>&1 > /dev/null
 then
     # ncat ships with nmap. It is the most modern and uniform netcat-like program
     #
     # Apparently ncat on the client side doesn't close the connection when receiving EOT,
     # so --send-only is needed
-    echo -en "$msg" | ncat --send-only localhost 9001
+    echo -en "$msg" | ncat --send-only localhost "$port"
 elif command -v socat 2>&1 > /dev/null
 then
     # Hopefully socat is more uniform than netcat. Tested on socat 1.7.3.4 on Arch. When
     # socat sees EOF on stdin it begins to close it's end of the socket, waiting for a bit
     # for responses.
-    echo -en "$msg" | socat - tcp:localhost:9001
+    echo -en "$msg" | socat - "tcp:localhost:$port"
 elif command -v nc 2>&1 > /dev/null
 then
     # nc is netcat. Implementations are ubiquitous, but fragmented.
@@ -38,13 +52,13 @@ then
     then
         # --close is undocumented, but is tested in gnu-netcat 0.7.1
         # See https://sourceforge.net/p/netcat/bugs/60/
-        echo -en "$msg" | nc --close localhost 9001
+        echo -en "$msg" | nc --close localhost "$port"
     elif echo -n "$ncHelp" | grep --quiet -- '-N' -
     then
         # openbsd-netcat requires -N (and also that the server send a FIN in response)
-        echo -en "$msg" | nc -N localhost 9001
+        echo -en "$msg" | nc -N localhost "$port"
     else
         # last-ditch effort, use -w, which apparently exists on Windows
-        echo -en "$msg" | nc -w 1 localhost 9001
+        echo -en "$msg" | nc -w 1 localhost "$port"
     fi
 fi
