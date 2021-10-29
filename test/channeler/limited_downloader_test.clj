697737f00287b3b3f512c8513b3a9b81ef55771b
Blzbrg <randomparanoid@gmail.com>
2021-10-28 22:43:48 -0400

    Timer loop and testing refactor
    - Rebuild the timer loop to use the time source to control the loop
      period, rather than relying on sleep time.
    - Pull expire/dequeue logic from the loop, and add new testing made
      possible by it.
    - Rebuild integration testing to take advantage of the new ability to
      delay the progress through the timer loop by mocking the time source.

--- test/channeler/limited_downloader_test.clj
+++ test/channeler/limited_downloader_test.clj
@@ -17,17 +17,46 @@
       (do (Thread/sleep 10)
           (recur cond-fn (- timeout 10))))))
 
-(test/deftest expire-asap-and-timed
+(test/deftest basic-expire-test
+   (let [asap {::dl/download-url "http://a.b/asap"}
+         timed {::dl/download-url "http://a.b/timed" ::dl/time-nano 1}
+         asap-ref (atom [asap])
+         sched-ref (atom (sorted-map 1 [timed]))]
+     (test/is (= asap
+                 (second (dl/timer-loop-dequeue-and-selection 0 clojure.lang.PersistentQueue/EMPTY
+                                                              sched-ref asap-ref))))
+     (test/is (= timed
+                 (second (dl/timer-loop-dequeue-and-selection 1 clojure.lang.PersistentQueue/EMPTY
+                                                              sched-ref asap-ref))))))
+
+(test/deftest expire-priority-test
+  ;; When a timed is eligible to expire and there is an asap in the queue, the timed should expire
+  ;; first.
+  (let [[mock-time-ref mock-time-fn] (make-mock-timer 0)
+        asap {::dl/download-url "http://a.b/asap"}
+        timed {::dl/download-url "http://a.b/timed" ::dl/time-nano 0}
+        asap-ref (atom [asap])
+        sched-ref (atom (sorted-map 0 [timed]))]
+    (test/is (= timed
+                (second (dl/timer-loop-dequeue-and-selection 0 clojure.lang.PersistentQueue/EMPTY
+                                                             sched-ref asap-ref))))
+    (test/is (= asap
+                (second (dl/timer-loop-dequeue-and-selection 0 clojure.lang.PersistentQueue/EMPTY
+                                                             sched-ref asap-ref))))))
+
+(test/deftest service-integration-test
   (let [[mock-time-ref mock-time-fn] (make-mock-timer 0)
         downloaded-reqs (atom [])
         asap {::dl/download-url "http://a.b/asap"}
-        timed {::dl/download-url "http://a.b/timed" ::dl/time-nano 0}]
+        timed {::dl/download-url "http://a.b/timed" ::dl/time-nano 1}]
     (with-redefs [dl/download-req! (fn [req] (swap! downloaded-reqs conj req))]
       (let [mock-ctx {:conf [{"network-rate-limit" {"min-sec-between-downloads" 0.1}}]}
             {svc ::dl/rate-limited-downloader} (dl/init-service mock-ctx mock-time-fn)]
         (service/handle-item! svc timed)
         (service/handle-item! svc asap)
-        ;; There is no way to know which request will be dequeued first, because the thread loop is
-        ;; running as soon as the service is created. When the time loop delay time is integrated
-        ;; with the time source this can be better controlled and a better test can be written.
-        (test/is (wait-for (fn [] (= (set [timed asap]) (set @downloaded-reqs))) 10000))))))
+        (test/is (wait-for (fn [] (= (set [asap]) (set @downloaded-reqs))) 5000))
+        (test/is (= (list timed) (get @(:sched-ref svc) 1)))
+        (reset! downloaded-reqs [])
+        (reset! mock-time-ref 1)
+        (test/is (wait-for (fn [] (set [timed]) (set @downloaded-reqs)) 5000))
+        (service/stop svc)))))
